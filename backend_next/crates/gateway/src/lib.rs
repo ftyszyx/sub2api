fbafbfbcd1535cae4c2550455cfd8274504335f9
ftyszyx <whyzi@qq.com>
2026-06-29 18:15:28 +0800

    recover

--- backend_next/crates/gateway/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod bridge;
-pub mod context;
-pub mod ingress;
-pub mod pipeline;
-pub mod provider;
-pub mod routing;
-pub mod state;
-
-pub use context::GatewayContext;
--- backend_next/crates/protocol/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod downstream;
-pub mod endpoint;
-
-pub use downstream::{DownstreamProtocol, ProtocolParseError};
-pub use endpoint::{GatewayEndpoint, GatewayEndpointKind, GatewayEndpointParseError};
